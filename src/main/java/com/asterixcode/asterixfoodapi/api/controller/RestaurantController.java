11241d5a36b0f200100be2cacbbd65566bdf06d9
asterixcode <lk.saldanha@gmail.com>
2021-05-16 23:20:53 +0200

    Implemented put and patch requests for restaurants

--- src/main/java/com/asterixcode/asterixfoodapi/api/controller/RestaurantController.java
+++ src/main/java/com/asterixcode/asterixfoodapi/api/controller/RestaurantController.java
@@ -4,12 +4,17 @@ import com.asterixcode.asterixfoodapi.domain.exception.EntityNotFoundException;
 import com.asterixcode.asterixfoodapi.domain.model.Restaurant;
 import com.asterixcode.asterixfoodapi.domain.repository.RestaurantRepository;
 import com.asterixcode.asterixfoodapi.domain.service.RestaurantRegisterService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/restaurants")
@@ -52,5 +57,75 @@ public class RestaurantController {
                     .badRequest() // 400 Bad Request
                     .body(e.getMessage());
         }
+    }
+
+    @PutMapping("/{restaurantId}")
+    public ResponseEntity<?> update(@PathVariable Long restaurantId,
+                                    @RequestBody Restaurant restaurant){
+        try {
+            // 1st- check the repository if restaurant exist by id
+            Restaurant restaurantCurrent = restaurantRepository.getBy(restaurantId);
+
+            if (restaurantCurrent != null) {
+                BeanUtils.copyProperties(restaurant, restaurantCurrent, "id");
+                restaurantRegisterService.save(restaurantCurrent);
+                return ResponseEntity.ok(restaurantCurrent); // 200 OK
+            } else {
+                return ResponseEntity.notFound().build(); // 404 Not Found
+            }
+        }
+
+        catch (EntityNotFoundException e) {
+            return ResponseEntity
+                    .badRequest() // 400 Bad Request
+                    .body(e.getMessage()); // kitchen_id does not exists
+        }
+    }
+
+    @PatchMapping("/{restaurantId}")
+    public ResponseEntity<?> updatePartial(@PathVariable Long restaurantId,
+                                           @RequestBody Map<String, Object> fields){
+        // check if id belongs to a restaurant
+        Restaurant restaurantCurrent = restaurantRepository.getBy(restaurantId);
+        // if not, 404 Not Found
+        if (restaurantCurrent == null){
+            return ResponseEntity.notFound().build(); // 404 Not Found
+        }
+
+        merge(fields, restaurantCurrent);
+
+        return update(restaurantId, restaurantCurrent);
+    }
+
+    private void merge(Map<String, Object> fieldsOrigin, Restaurant restaurantDestiny) {
+        // ** 1st:
+        // we need to convert the field to its specific type (String, BigDecimal, other objects...)
+        // ObjectMapper from jackson -> responsible for Serializations (Java objects to JSON, JSON to Java obj)
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Convert the origin fields/data from the Map to a type=Restaurant.class
+        // Create an instance of type Restaurant.class using as base the Map with origin data/fields
+        Restaurant restaurantOrigin = objectMapper.convertValue(fieldsOrigin, Restaurant.class);
+
+        // ** 2nd:
+        // with the values from the JSON converted and matching the fields of a Restaurant.class,
+        // now we need to assign/merge/patch only the values that was updated.
+        // It's not possible to merge all at once, we need to check and merge only updated values,
+        // this way we need the origin fields and
+        // get the fields from restaurantOrigin and set into the restaurantDestiny dinamically
+        fieldsOrigin.forEach((propertyName, propertyValue) -> {
+            // 1st: findField = return an instance of a field
+            Field field = ReflectionUtils.findField(Restaurant.class, propertyName);
+            field.setAccessible(true);
+
+            // 2nd: getField = get the value of that field finded above
+            Object newValue = ReflectionUtils.getField(field, restaurantOrigin);
+
+            System.out.println(propertyName + " = " + propertyValue + " = " + newValue);
+            // 3rd: set the newValue of the field (already converted to its correct field type)
+            // into the restaurantDestiny
+            ReflectionUtils.setField(field, restaurantDestiny, newValue);
+        });
+
     }
 }
--- src/main/java/com/asterixcode/asterixfoodapi/domain/service/RestaurantRegisterService.java
+++ src/main/java/com/asterixcode/asterixfoodapi/domain/service/RestaurantRegisterService.java
@@ -18,18 +18,23 @@ public class RestaurantRegisterService {
     private KitchenRepository kitchenRepository;
 
     public Restaurant save(Restaurant restaurant){
+        // 1st- get the kitchen id inside of the restaurant sent
         Long kitchenId = restaurant.getKitchen().getId();
 
-        // 1st - check if the kitchen exists
+        // 2nd- check if the kitchen exists by the above id
         Kitchen kitchen = kitchenRepository.getBy(kitchenId);
+
+        // 3rd- condition result for the above search/check
         if(kitchen == null) {
             throw new EntityNotFoundException(
                     String.format("Kitchen with Id %d does not exist", kitchenId));
             // the EntityNotFoundException will be thrown in case kitchen does not exists,
             // and Exception has to be handled in the controller with a try/catch
         }
+        // set the kitchen found to the restaurant sent
         restaurant.setKitchen(kitchen);
 
+        // add the restaurant sent to restaurant table
         return restaurantRepository.add(restaurant);
     }
 }
