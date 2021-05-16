005472240e48eac76e485411befc3643adfd31bf
asterixcode <lk.saldanha@gmail.com>
2021-05-16 17:14:32 +0200

    Removed XML support example

--- src/main/java/com/asterixcode/asterixfoodapi/api/controller/KitchenController.java
+++ src/main/java/com/asterixcode/asterixfoodapi/api/controller/KitchenController.java
@@ -1,6 +1,5 @@
 package com.asterixcode.asterixfoodapi.api.controller;
 
-import com.asterixcode.asterixfoodapi.api.model.KitchenXmlWrapper;
 import com.asterixcode.asterixfoodapi.domain.exception.EntityInUseException;
 import com.asterixcode.asterixfoodapi.domain.exception.EntityNotFoundException;
 import com.asterixcode.asterixfoodapi.domain.model.Kitchen;
@@ -28,16 +27,9 @@ public class KitchenController {
     @Autowired
     private KitchenRegisterService kitchenRegisterService;
 
-    // return kitchen list as JSON (Spring standard)
     @GetMapping
     public List<Kitchen> listAll() {
         return kitchenRepository.listAll();
-    }
-
-    // return kitchen list as XML (as example)
-    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public KitchenXmlWrapper listAllXml() {
-        return new KitchenXmlWrapper(kitchenRepository.listAll());
     }
 
     @GetMapping("/{kitchenId}")
