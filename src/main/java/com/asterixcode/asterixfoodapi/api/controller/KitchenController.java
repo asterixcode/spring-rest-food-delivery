9b1543874119e3306ef56d08692e5d63bb610b58
asterixcode <lk.saldanha@gmail.com>
2021-05-16 12:32:48 +0200

    Added XML return type example for listAll kitchens

--- src/main/java/com/asterixcode/asterixfoodapi/api/controller/KitchenController.java
+++ src/main/java/com/asterixcode/asterixfoodapi/api/controller/KitchenController.java
@@ -1,13 +1,12 @@
 package com.asterixcode.asterixfoodapi.api.controller;
 
+import com.asterixcode.asterixfoodapi.api.model.KitchenXmlWrapper;
 import com.asterixcode.asterixfoodapi.domain.model.Kitchen;
 import com.asterixcode.asterixfoodapi.domain.repository.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +17,20 @@ public class KitchenController {
     @Autowired
     private KitchenRepository kitchenRepository;
 
+    // return kitchen list as JSON (Spring standard)
     @GetMapping
     public List<Kitchen> listAll() {
         return kitchenRepository.listAll();
+    }
+
+    // return kitchen list as XML (as example)
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public KitchenXmlWrapper listAllXml() {
+        return new KitchenXmlWrapper(kitchenRepository.listAll());
+    }
+
+    @GetMapping("/{kitchenId}")
+    public Kitchen getBy(@PathVariable("kitchenId") Long id){
+        return kitchenRepository.getBy(id);
     }
 }
